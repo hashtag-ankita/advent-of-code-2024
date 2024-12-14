a6827dbe4b0a2a0817a712af9951df41b8179e7f
Ankita Dey <138559097+hashtag-ankita@users.noreply.github.com>
2024-12-14 15:16:43 +0530

    Added all input.txt files into .gitignore (since input are unique for all users)

--- Day 5/Day5.py
+++ Day 5/Day5.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 class Day5:
     def part1(self, rules, updates):
         middleSum = 0
@@ -10,7 +11,6 @@ class Day5:
             for i, num in enumerate(update):
                 idx[num] = i
 
-
             for a, b in rules:
                 if a in idx and b in idx:
                     if idx[a] > idx[b]:
@@ -21,6 +21,42 @@ class Day5:
                 middleSum += update[n//2]
 
         return middleSum
+    
+    def part2(self, rules, updates):
+        totalMidSum = 0 # the total mid sum of the incorrect updates after correctly ordering them
+
+        for update in updates:
+            n = len(update)
+
+            # filtering out rules as per the current update
+            current_rules = []
+            for a, b in rules:
+                if a in update and b in update:
+                    current_rules.append((a, b))
+
+            # finding the indegrees of each number in the update (to find number of dependencies for each page, if any)
+            indeg = defaultdict(int)
+            for a, b in current_rules:
+                indeg[b] += 1
+
+            # finding the correct order of the update
+            correct_order = []
+
+            while len(correct_order) < len(update):
+                for x in update:
+                    if x in correct_order:
+                        continue
+                    if indeg[x] <= 0:
+                        correct_order.append(x)
+                        for a, b in current_rules:
+                            if a == x:
+                                indeg[b] -= 1
+
+            # finding the mid sum of the correct orders
+            totalMidSum += correct_order[n//2]
+            
+
+        return totalMidSum
 
     @staticmethod
     def main():
@@ -42,7 +78,10 @@ class Day5:
             Day5_instance = Day5()
 
             middleSum = Day5_instance.part1(rules, updates)
-            print(middleSum)
+            print(f"Middle sum of the correct updates: {middleSum}")
+
+            totalMidSum = Day5_instance.part2(rules, updates) - middleSum
+            print(f"Middle sum of the incorrect updates, after correction: {totalMidSum}")
 
 
 if __name__ == "__main__":
